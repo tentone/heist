ed324a6beb47c3d901d11870320e9753562148a5
Tentone <tentone@outlook.com>
2017-06-03 00:36:57 +0100

    Removed tests

--- source/src/heist/rmi/backengine/registry/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- *  Registration of remote objects in the local registry service.<p>
- *  Back engine application: code is moved and executed in a platform supposed to be more powerful.
- *  Communication is based in Java RMI.
- */
-
-package heist.rmi.backengine.registry;
